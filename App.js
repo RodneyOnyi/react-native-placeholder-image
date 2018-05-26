9d66102c5b4d832360ef4c7fb788b3ca083138c1
nkshah2 <nemishah1212@gmail.com>
2018-05-26 15:48:23 +0530

    Add and configure eslint and flow

--- App.js
+++ App.js
@@ -12,25 +12,25 @@ import {
   View
 } from 'react-native';
 
-const instructions = Platform.select({
+const instructions = Platform.select( {
   ios: 'Press Cmd+R to reload,\n' +
     'Cmd+D or shake for dev menu',
   android: 'Double tap R on your keyboard to reload,\n' +
     'Shake or press menu button for dev menu',
-});
+} );
 
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
+      <View style={ styles.container }>
+        <Text style={ styles.welcome }>
           Welcome to React Native!
         </Text>
-        <Text style={styles.instructions}>
+        <Text style={ styles.instructions }>
           To get started, edit App.js
         </Text>
-        <Text style={styles.instructions}>
+        <Text style={ styles.instructions }>
           {instructions}
         </Text>
       </View>
@@ -38,7 +38,7 @@ export default class App extends Component<Props> {
   }
 }
 
-const styles = StyleSheet.create({
+const styles = StyleSheet.create( {
   container: {
     flex: 1,
     justifyContent: 'center',
@@ -55,4 +55,4 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
-});
+} );
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
 
-AppRegistry.registerComponent('ReactNativeImagePlaceholder', () => App);
+AppRegistry.registerComponent( 'ReactNativeImagePlaceholder', () => App );
